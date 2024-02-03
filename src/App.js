82c2ad7f57e3ad45b65fe9694986d38ef5fabd3e
unknown <chohankoti333@gmail.com>
2024-02-03 11:26:15 +0530

    trying to add certificate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Details from "./Details";
 import IndividualDetails from "./IndividualDetails";
+import Certificate from "./Certificate";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Details />} />
         <Route path="/details/:id" element={<IndividualDetails />} />
+        <Route path="/certificate/:id" element={<Certificate/>}/>
       </Routes>
     </Router>
   );
