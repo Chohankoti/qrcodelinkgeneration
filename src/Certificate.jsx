3b988b942a1bacd134c8ee9f41e2f426efcfda07
unknown <chohankoti333@gmail.com>
2024-02-03 11:41:28 +0530

    some error are occuring try later

--- src/Certificate.jsx
+++ src/Certificate.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { exportComponentAsPNG } from "react-component-export-image";
+import { useParams } from "react-router-dom";
 import "./Certificate.css";
 import familyData from "./familyData";
 
 const Certificate = () => {
+  const { id } = useParams();
+  const member = familyData.find((m) => m.id === parseInt(id, 10));
   const handleDownload = () => {
     exportComponentAsPNG(document.getElementById("certificateWrapper"), {
       html2CanvasOptions: { backgroundColor: null },
@@ -12,15 +15,13 @@ const Certificate = () => {
 
   return (
     <div className="Certificate">
-      {familyData.map((member, index) => (
-        <div key={index} className="certificateWrapper">
+        <div className="certificateWrapper">
           <p>{member.name}</p>
           <img src="https://i.imgur.com/zMFcb3kh.png" alt="Certificate" />
           <div className="Meta">
             <button onClick={handleDownload}>Download</button>
           </div>
         </div>
-      ))}
     </div>
   );
 };
