39c0e436ef560a55610742ed85daa14c2463cbe7
unknown <chohankoti333@gmail.com>
2024-02-03 11:34:04 +0530

    almost done to add certificate

--- src/Certificate.jsx
+++ src/Certificate.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { exportComponentAsPNG } from "react-component-export-image";
+import "./Certificate.css";
+import familyData from "./familyData";
 
-export default function Certificate() {
+const Certificate = () => {
+  const handleDownload = () => {
+    exportComponentAsPNG(document.getElementById("certificateWrapper"), {
+      html2CanvasOptions: { backgroundColor: null },
+    });
+  };
+
   return (
-    <div>Certificate</div>
-  )
-}
+    <div className="Certificate">
+      {familyData.map((member, index) => (
+        <div key={index} className="certificateWrapper">
+          <p>{member.name}</p>
+          <img src="https://i.imgur.com/zMFcb3kh.png" alt="Certificate" />
+          <div className="Meta">
+            <button onClick={handleDownload}>Download</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Certificate;
